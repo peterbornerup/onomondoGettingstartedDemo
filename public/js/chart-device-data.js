bbada6f6bba16c60a2417d9a7015adce7d2e24aa
peterbornerup <peter.bornerup@outlook.dk>
2020-08-11 10:33:26 +0200

    modified working version

---
+++ public/js/chart-device-data.js
@@ -0,0 +1,209 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
+$(document).ready(() => {
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  const simIDUser = urlParams.get('simID');
+
+
+  // if deployed to a site supporting SSL, use wss://
+  const protocol = document.location.protocol.startsWith('https') ? 'wss://' : 'ws://';
+  const webSocket = new WebSocket(protocol + location.host);
+  console.log(location.host)
+  // A class for holding the last N points of telemetry for a device
+  class DeviceData {
+    constructor(deviceId) {
+      this.deviceId = deviceId;
+      this.maxLen = 50;
+      this.timeData = new Array(this.maxLen);
+      this.voltageData = new Array(this.maxLen);
+      this.signalData = new Array(this.maxLen);
+    }
+
+    addData(time, voltage, signal) {
+      this.timeData.push(time);
+      this.voltageData.push(voltage);
+      this.signalData.push(signal || null);
+
+      if (this.timeData.length > this.maxLen) {
+        this.timeData.shift();
+        this.voltageData.shift();
+        this.signalData.shift();
+      }
+    }
+  }
+
+  // All the devices in the list (those that have been sending telemetry)
+  class TrackedDevices {
+    constructor() {
+      this.devices = [];
+    }
+
+    // Find a device based on its Id
+    findDevice(deviceId) {
+      for (let i = 0; i < this.devices.length; ++i) {
+        if (this.devices[i].deviceId === deviceId) {
+          return this.devices[i];
+        }
+      }
+
+      return undefined;
+    }
+
+    getDevicesCount() {
+      return this.devices.length;
+    }
+  }
+
+  const trackedDevices = new TrackedDevices();
+
+  // Define the chart axes
+  const chartData = {
+    datasets: [
+      {
+        fill: false,
+        label: 'Voltage',
+        yAxisID: 'Voltage',
+        borderColor: 'rgba(171,125,99,1)',
+        pointBoarderColor: 'rgba(171,125,99, 1)',
+        backgroundColor: 'rgba(171,125,99, 0.4)',
+        pointHoverBackgroundColor: 'rgba(171,125,99, 1)',
+        pointHoverBorderColor: 'rgba(171,125,99, 1)',
+        spanGaps: true,
+      },
+      {
+        fill: false,
+        label: 'Signal',
+        yAxisID: 'Signal',
+        borderColor: 'rgba(255, 255, 255, 1)',
+        pointBoarderColor: 'rgba(255, 255, 255, 1)',
+        backgroundColor: 'rgba(255, 255, 255, 0.4)',
+        pointHoverBackgroundColor: 'rgba(24, 120, 240, 1)',
+        pointHoverBorderColor: 'rgba(24, 120, 240, 1)',
+        spanGaps: true,
+      }
+    ]
+  };
+
+  const chartOptions = {
+    scales: {
+      xAxes: [{
+        time: {
+          displayFormats: {
+              quarter: 'MMM YYYY'
+          }
+      }
+      }],
+      yAxes: [{
+        id: 'Voltage',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'Battery voltage',
+          display: true,
+        },
+        position: 'left',
+      },
+      {
+        id: 'Signal',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'Signal quality',
+          display: true,
+        },
+        position: 'right',
+      }]
+    },
+  };
+
+  // Get the context of the canvas element we want to select
+  const ctx = document.getElementById('iotChart').getContext('2d');
+  const myLineChart = new Chart(
+    ctx,
+    {
+      type: 'line',
+      data: chartData,
+      options: chartOptions,
+    });
+
+  // Manage a list of devices in the UI, and update which device data the chart is showing
+  // based on selection
+  let needsAutoSelect = true;
+  const deviceCount = document.getElementById('deviceCount');
+  const listOfDevices = document.getElementById('listOfDevices');
+  function OnSelectionChange() {
+    const device = trackedDevices.findDevice(listOfDevices[listOfDevices.selectedIndex].text);
+    console.log(listOfDevices[listOfDevices.selectedIndex].text)
+    console.log("Found device:");
+    console.log(device);
+
+    chartData.labels = device.timeData;
+    chartData.datasets[0].data = device.voltageData;
+    chartData.datasets[1].data = device.signalData;
+    myLineChart.update();
+  }
+ // listOfDevices.addEventListener('change', OnSelectionChange, false);
+  
+  // When a web socket message arrives:
+  // 1. Unpack it
+  // 2. Validate it has date/time and temperature
+  // 3. Find or create a cached device to hold the telemetry data
+  // 4. Append the telemetry data
+  // 5. Update the chart UI
+  webSocket.onmessage = function onMessage(message) {
+    try {
+      const messageData = JSON.parse(message.data);
+
+      // time and either temperature or humidity are required
+      if (!messageData.MessageDate || (!messageData.IotData.payload.battery && !messageData.IotData.payload.signal)) {
+        return;
+      }
+
+      
+      // find or add device to list of tracked devices
+      var deviceID = messageData.IotData.sim_id.toString();
+      var battery = messageData.IotData.payload.battery;
+      var signal = messageData.IotData.payload.signal;
+      
+      
+      if(deviceID != simIDUser){
+      console.log(deviceID, simIDUser);
+
+
+        console.log("Wrong id");
+        return;
+      }
+
+
+      const existingDeviceData = trackedDevices.findDevice(deviceID);
+
+
+
+      if (existingDeviceData) {
+        existingDeviceData.addData(Date(messageData.MessageDate), battery,signal);
+      } else {
+        const newDeviceData = new DeviceData(deviceID);
+        const numDevices = trackedDevices.getDevicesCount();
+        trackedDevices.devices.push(newDeviceData);
+
+
+
+
+        newDeviceData.addData(Date(messageData.MessageDate), battery, signal);
+
+        if(numDevices == 0){
+          //then we wish to add the data..
+          chartData.labels = newDeviceData.timeData;
+          chartData.datasets[0].data = newDeviceData.voltageData;
+          chartData.datasets[1].data = newDeviceData.signalData;
+        }
+      }
+
+      myLineChart.update();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+});
---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+const EventHubReader = require('./scripts/event-hub-reader.js');
+const moment = require('moment')
+
+const iotHubConnectionString = process.env.IotHubConnectionString;
+if (!iotHubConnectionString) {
+  console.error(`Environment variable IotHubConnectionString must be specified.`);
+  return;
+}
+console.log(`Using IoT Hub connection string [${iotHubConnectionString}]`);
+
+
+
+
+const eventHubConsumerGroup = process.env.EventHubConsumerGroup;
+console.log(eventHubConsumerGroup);
+if (!eventHubConsumerGroup) {
+  console.error(`Environment variable EventHubConsumerGroup must be specified.`);
+  return;
+}
+console.log(`Using event hub consumer group [${eventHubConsumerGroup}]`);
+
+// Redirect requests to the public subdirectory to the root
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/:id', (req, res) => {
+  console.log(req.params);
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+})
+
+
+
+app.use((req, res /* , next */) => {
+  console.log("redirect..");
+  res.redirect('/');
+});
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.broadcast = (data) => {
+  console.log("Clients:");
+  console.log(wss.clients.size);
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      try {
+        console.log(`Broadcasting data ${data}`);
+        client.send(data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+};
+
+server.listen(process.env.PORT || '3000', () => {
+  console.log('Listening on %d.', server.address().port);
+});
+
+const eventHubReader = new EventHubReader(iotHubConnectionString, eventHubConsumerGroup);
+
+(async () => {
+  await eventHubReader.startReadMessage((message, date, deviceId) => {
+    try {
+      const payload = {
+        IotData: message,
+        MessageDate: date || Date.now().toISOString(),
+        DeviceId: deviceId,
+      }
+      wss.broadcast(JSON.stringify(payload));
+    } catch (err) {
+      console.error('Error broadcasting: [%s] from [%s].', err, message);
+    }
+  });
+})().catch();
